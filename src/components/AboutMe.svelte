ad01ac3865cf3d6293bf637840c0dbbb3d56a039
wu-json <jason.wu2153@gmail.com>
2023-09-26 15:04:30 -0700

    feat: caption

--- src/components/AboutMe.svelte
+++ src/components/AboutMe.svelte
@@ -1,6 +1,11 @@
 <div class="wrapper">
   <div class="content">
     <h1 class="pink-gradient-text">about me</h1>
+    <p>
+      Hey there! I'm a voice actor looking for roles in video games or anime
+      dubs. I have a deep bass-heavy voice and am able to voice characters in
+      both English and Mandarin Chinese.
+    </p>
   </div>
 </div>
 
@@ -18,9 +23,15 @@
     left: 50%;
     transform: translate(-50%, -50%);
     display: flex;
+    flex-direction: column;
     align-items: center;
+    max-width: 600px;
   }
   h1 {
-    font-size: 40px;
+    font-size: 60px;
+    margin: 0px;
+  }
+  p {
+    font-size: 18px;
   }
 </style>
