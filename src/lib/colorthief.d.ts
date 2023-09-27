ae4740d0754b3e161c9db1e3806012bdb9b8e004
wu-json <jason.wu2153@gmail.com>
2023-09-27 16:15:09 -0700

    feat: colorthief types

--- src/lib/colorthief.d.ts
+++ src/lib/colorthief.d.ts
@@ -1,1 +1,8 @@
-declare module 'colorthief';
+// Courtesy of: https://github.com/lokesh/color-thief/issues/188
+declare module 'colorthief' {
+  type Color = [number, number, number];
+  export default class ColorThief {
+    getColor: (img: HTMLImageElement | null) => Color;
+    getPalette: (img: HTMLImageElement | null) => Color[];
+  }
+}
