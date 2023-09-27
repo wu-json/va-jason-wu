5504b4e3382d41a2e94c234510f13e4193d8a291
wu-json <jason.wu2153@gmail.com>
2023-09-26 17:00:45 -0700

    fix: safari gradient bug

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -14,6 +14,7 @@
     color: #ffffff;
   }
   :global(.blue-gradient-text) {
+    clip-path: inset(1px);
     background-color: #0038ff;
     background-image: linear-gradient(45deg, #0038ff, #22f2ff);
     background-size: 100%;
@@ -24,6 +25,7 @@
     -moz-text-fill-color: transparent;
   }
   :global(.pink-gradient-text) {
+    clip-path: inset(1px);
     background-color: #ff00f5;
     background-image: linear-gradient(45deg, #ff00f5, #9104ff);
     background-size: 100%;
@@ -34,6 +36,7 @@
     -moz-text-fill-color: transparent;
   }
   :global(.green-gradient-text) {
+    clip-path: inset(1px);
     background-color: #00ff1a;
     background-image: linear-gradient(45deg, #00ff1a, #04ffd2);
     background-size: 100%;
